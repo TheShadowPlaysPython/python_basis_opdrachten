fb0830522a8219e93dad674c9516514930e727ba
Timo Pluim <hendriktimopluim@gmail.com>
2024-03-14 14:39:56 +0100

    kkr

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,23 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = -12
 
-print()
+
+f = (c * 1.8) + 32
+print("Temperatuur in Fahrenheit is ", f, "F")
+
+f = 102
+c = (f - 32) / 1.8
+print("Temperatuur in Celsius is ", c, " graden")
+
+c = 62.2
+f = 96
+
+f = (c * 1.8) + 32
+print("Temperatuur in Fahrenheit is ", f, "F")
+
+c = (f - 32) / 1.8
+print("Temperatuur in Celsius is ", c, " graden")
+
+
